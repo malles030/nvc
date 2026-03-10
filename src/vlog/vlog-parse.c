6820e2923a315791d40a9d0a17a640a81d8b9904
Matthias Alles <218066411+malles030@users.noreply.github.com>
2026-03-10 15:55:53 +0100

    Fix Verilog parsing error while using parameters

--- src/vlog/vlog-parse.c
+++ src/vlog/vlog-parse.c
@@ -6483,6 +6483,7 @@ static void p_module_ansi_header(vlog_node_t mod)
    EXTEND("module ANSI header");
 
    if (peek() == tHASH) {
+      // Parameters not yet parsed (no preceding non-ANSI check)
       p_parameter_port_list(mod);
       param_kind = V_LOCALPARAM;
    }
@@ -6587,6 +6588,11 @@ static vlog_node_t p_module_declaration(void)
 
    while (peek() == tIMPORT)
       p_package_import_declaration(mod);
+
+   if (peek() == tHASH) {
+      p_parameter_port_list(mod);
+      param_kind = V_LOCALPARAM;
+   }
 
    if (peek() == tLPAREN && peek_nth(2) == tID)
       p_module_nonansi_header(mod);
